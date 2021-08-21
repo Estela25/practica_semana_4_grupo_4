7c457a0d2d962bc56e1200cf1c375f49cdb680c8
Estela25 <88106388+Estela25@users.noreply.github.com>
2021-08-21 17:48:59 -0600

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -11,8 +11,7 @@ using System.Windows.Forms;
 namespace MatchingGame
 {
     public partial class Form1 : Form
-    {
-        // firstClicked points to the first Label control 
+    { 
         // that the player clicks, but it will be null 
         // if the player hasn't clicked a label yet
         Label firstClicked = null;
