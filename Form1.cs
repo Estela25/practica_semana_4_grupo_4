0c9822a341fdc3d9264ecb16f2cd52461ed3b406
Estela25 <USIS035821@ugb.edu.sv>
2021-08-21 18:52:22 -0600

    ook

--- Form1.cs
+++ Form1.cs
@@ -12,8 +12,6 @@ namespace MatchingGame
 {
     public partial class Form1 : Form
     { 
-        // that the player clicks, but it will be null 
-        // if the player hasn't clicked a label yet
         Label firstClicked = null;
 
         // secondClicked points to the second Label control 
